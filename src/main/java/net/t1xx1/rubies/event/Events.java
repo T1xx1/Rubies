6f4a4a5e1ed1c017fc0a4ccb6cc4f74393245b23
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-01-21 20:31:52 +0100

    feat: armorer and butcher trades

--- src/main/java/net/t1xx1/rubies/event/Events.java
+++ src/main/java/net/t1xx1/rubies/event/Events.java
@@ -23,7 +23,32 @@ public class Events {
         if (event.getType() == VillagerProfession.ARMORER) {
             Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
 
-            trades.get(1).add((entity, randomSource) -> new MerchantOffer(new ItemCost(Items.RUBY.get(), 1), new ItemStack(Blocks.RUBY_BLOCK.get(), 1), 2, 3, 0.05F));
+            trades.get(1).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.COAL, 20), new ItemStack(Items.RUBY.get(), 1), 16, 2, 0.05F));
+
+            trades.get(2).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.IRON_INGOT, 6), new ItemStack(Items.RUBY.get(), 1), 12, 10, 0.2F));
+
+            trades.get(3).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.DIAMOND, 2), new ItemStack(Items.RUBY.get(), 1), 12, 20, 0.05F));
+            trades.get(3).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.EMERALD, 1), new ItemStack(Items.RUBY.get(), 1), 12, 20, 0.05F));
+        }
+
+        if (event.getType() == VillagerProfession.BUTCHER) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+
+            trades.get(1).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.CHICKEN, 18), new ItemStack(Items.RUBY.get(), 1), 16, 2, 0.05F));
+            trades.get(1).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.RABBIT, 4), new ItemStack(Items.RUBY.get(), 1), 16, 2, 0.05F));
+            trades.get(1).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.PORKCHOP, 9), new ItemStack(Items.RUBY.get(), 1), 16, 2, 0.05F));
+            trades.get(1).add((entity, randomSource) -> new MerchantOffer(new ItemCost(Items.RUBY.get(), 18), new ItemStack(net.minecraft.world.item.Items.RABBIT_STEW, 2), 12, 1, 0.05F));
+
+            trades.get(2).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.COAL, 20), new ItemStack(Items.RUBY.get(), 1), 16, 2, 0.05F));
+            trades.get(2).add((entity, randomSource) -> new MerchantOffer(new ItemCost(Items.RUBY.get(), 1), new ItemStack(net.minecraft.world.item.Items.COOKED_CHICKEN, 10), 16, 5, 0.05F));
+            trades.get(2).add((entity, randomSource) -> new MerchantOffer(new ItemCost(Items.RUBY.get(), 1), new ItemStack(net.minecraft.world.item.Items.COOKED_PORKCHOP, 7), 16, 5, 0.05F));
+
+            trades.get(3).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.BEEF, 12), new ItemStack(Items.RUBY.get(), 1), 16, 20, 0.05F));
+            trades.get(3).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.MUTTON, 10), new ItemStack(Items.RUBY.get(), 1), 16, 20, 0.05F));
+
+            trades.get(4).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.DRIED_KELP, 12), new ItemStack(Items.RUBY.get(), 1), 12, 30, 0.05F));
+
+            trades.get(5).add((entity, randomSource) -> new MerchantOffer(new ItemCost(net.minecraft.world.item.Items.SWEET_BERRIES, 12), new ItemStack(Items.RUBY.get(), 1), 12, 30, 0.05F));
         }
     }
 
